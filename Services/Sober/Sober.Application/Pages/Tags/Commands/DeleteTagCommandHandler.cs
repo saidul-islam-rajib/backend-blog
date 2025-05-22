cfe3fd59b5eeb845e7ab8fc1bf942c73c7ebe5e7
Rajib <saidulislamrajib1997@gmail.com>
2025-05-22 11:46:26 +0600

    add delete cache

--- Services/Sober/Sober.Application/Pages/Tags/Commands/DeleteTagCommandHandler.cs
+++ Services/Sober/Sober.Application/Pages/Tags/Commands/DeleteTagCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BuildingBlocks.Exceptions;
 using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
 using Sober.Application.Interfaces;
 using Sober.Domain.Aggregates.TagAggregates;
 
@@ -8,10 +9,13 @@ namespace Sober.Application.Pages.Tags.Commands;
 public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand, bool>
 {
     private readonly ITagRepository _tagRepository;
+    private readonly IDistributedCache _cache;
 
-    public DeleteTagCommandHandler(ITagRepository tagRepository)
+
+    public DeleteTagCommandHandler(ITagRepository tagRepository, IDistributedCache cache)
     {
         _tagRepository = tagRepository;
+        _cache = cache;
     }
 
     public async Task<bool> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
@@ -22,6 +26,15 @@ public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand, bool>
             throw new NotFoundException($"Tag with ID {request.tagId} not found.");
         }
         bool isDeleted = _tagRepository.DeleteTag(request.tagId);
+        if (isDeleted)
+        {
+            // Remove individual tag from cache
+            string tagCacheKey = $"tag:{request.tagId}";
+            await _cache.RemoveAsync(tagCacheKey, cancellationToken);
+
+            // Invalidate the tag list cache
+            await _cache.RemoveAsync("tags:all", cancellationToken);
+        }
 
         return isDeleted;
     }
